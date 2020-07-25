23592e67d782a036a73fd730040dde32d484794a
kneetoe <robertopalmaliberty@gmail.com>
2020-07-25 16:16:12 -0700

    Add files via upload
    Added function to seek only tweets that are in English.

---
+++ twitter_scrape.py
@@ -0,0 +1,116 @@
+from twitterscraper import query_tweets
+from langdetect import detect
+import csv
+import datetime as dt
+
+
+dataFields = {
+    "text": 0,
+    "hashtags": 0,
+    "likes" : 0,
+    "has_media" : 0,
+    "retweets" : 0,
+    "img_urls" : 0,
+    "screen_name": 0,
+    "timestamp" : 0,
+    "is_replied" : 0,
+    "is_reply_to" : 0,
+    "parent_tweet_id" : 0,
+    "replies" : 0,
+    "tweet_id" : 0,
+    "tweet_url" : 0,
+    "user_id" : 0,
+    "username" : 0
+    #"video_url" : 0
+}
+'''
+# Helper function #
+
+Returns boolean that checks if the language
+    in the text is in english
+
+'''
+def language(text):
+    return ('en' == detect(text))
+
+if __name__ == '__main__':
+    
+    #how many days ago you want to retrieve tweets from
+    num_Days_ago = 2 
+    
+    # Takes tweets from date
+    date = dt.date.today() - dt.timedelta(days=num_Days_ago)
+
+    limit = 1000
+    lang = 'english'
+
+    with open('tweets_' +
+              str(date) +
+              '.csv', 'w',
+              newline='',
+              encoding="utf-8") as file:
+        
+        writer = csv.writer(file)
+        fields = []
+        count = 0
+        #write first row
+        for x in dataFields:
+            fields.append(x)
+        writer.writerow(fields)
+        
+        tomorrow = date + dt.timedelta(days=1)
+        for tweet in query_tweets("#NBA",
+                              limit = limit,
+                              begindate=date,
+                              enddate=tomorrow,
+                              lang = 'english'):
+            #Store Tweets without videos
+            if(tweet.video_url == '' and language(tweet.text)):
+            
+                #Gather info for row
+                fields = []
+                
+                fields.append(tweet.text)           # string
+                
+                #array type hashTags
+                hashtags = ''
+                for hashtag in (tweet.hashtags):
+                    if (hashtags == ''):
+                        hashtags = hashtag
+                    else:
+                        hashtags = hashtags + '|' + hashtag
+                fields.append(hashtags)
+                
+                fields.append(tweet.likes)          # int
+                fields.append(tweet.has_media)      # bool
+                fields.append(tweet.retweets)       # int
+
+                #array type URL
+                urls = ''
+                for url in (tweet.img_urls):
+                    if (urls == ''):
+                        urls = url
+                    else:    
+                        urls = url + '|' + urls
+                fields.append(urls)
+                
+                fields.append(tweet.screen_name)    # string
+                fields.append(tweet.timestamp)      # string
+                fields.append(tweet.is_replied)     # bool
+                fields.append(tweet.is_reply_to)    # bool
+                fields.append(tweet.parent_tweet_id)# string
+                fields.append(tweet.replies)        # int
+                fields.append(tweet.tweet_id)       # string
+                fields.append(tweet.tweet_url)      # string
+                fields.append(tweet.user_id)        # int
+                fields.append(tweet.username)       # string
+
+                #write row
+                writer.writerow(fields)
+                count = count + 1
+
+    print("Num Found: " + str(count))
+
+                
+        
+
